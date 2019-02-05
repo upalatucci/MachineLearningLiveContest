bc7f6cfab72d68dfaf4bb0b91b5aa700b7b556b0
upalatucci <u.palatucci@studenti.unisa.it>
2019-02-05 09:11:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ STEPS = 10
 if __name__ == '__main__':
     labels = ["Risata", "Pianto bimbo", "Tosse", "Starnuto", "Passi, camminare", "Applauso", "Cane", "Gatto", "Acqua", "Motore", "Sveglia", "Sirena"]
     model = load_model("../tot_model_1024_04_256.h5")
+    report_file = open("../report.txt", "w")
     extractor = Extractor()
     record_thread = RecordandSaveThread(steps=STEPS)
     record_thread.start()
@@ -27,10 +28,14 @@ if __name__ == '__main__':
         array_features = np.append(array_features, features, axis=0)
 
         if np.shape(array_features)[0] == STEPS:
+            string_to_print = "=========================\n"
             predictions = model.predict(np.array([array_features]))[0]
-            print("Prediction: {}".format(predictions))
 
             for i in range(len(predictions)):
                 if predictions[i] > confidence:
-                    print("Label: {}".format(labels[i]))
-            array_features = np.empty((0, 128))
+                    string_to_print += "====>"
+                string_to_print += " {}: {}\n".format(labels[i], predictions[i])
+
+            print(string_to_print)
+            report_file.write(string_to_print)
+            array_features = np.delete(array_features, 0, 0)
