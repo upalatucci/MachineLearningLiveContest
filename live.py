1dfd67dcd59918192d8bdb12e6bebd1bc5464412
upalatucci <u.palatucci@studenti.unisa.it>
2019-02-16 16:02:55 +0100

    Add files via upload

--- live.py
+++ live.py
@@ -1,5 +1,5 @@
 """
-    At the start, the window have size 0. Every second, the programm read the wav file create from the ReadandSaveThread
+    At the start, the window have size 0. Every second, the programm read the wav file created from the ReadandSaveThread
     and convert the audio information into features using the Extractor.
 
     So, when the window have the minimum size to be processed from the CustomMulticlassifier, the prediction begin.
@@ -21,6 +21,7 @@ import numpy as np
 from custom_multiclassifier import CustomMulticlassifier
 from extract_audioset_embedding import Extractor
 from record_and_save import RecordandSaveThread
+import sys 
 
 labels = {
     16: "Risata",
@@ -40,10 +41,28 @@ labels = {
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--configuration_models", help="Path of configuration models. Default is configuration.json", default=os.path.join(os.getcwd(), "configuration.json"))
+    parser.add_argument("-x", "--confidence", help="The probability of the prediction should have a value bigger than this number to show the result", default=0.2)
+    parser.add_argument("-w", "--window", help="Maximum window to consider for the prediction.", default=10)
+    
     configuration_path = parser.parse_args().configuration_models
+
+    try:
+        confidence = float(parser.parse_args().confidence)
+    except:
+        print("Confidence should be a number")
+        sys.exit()
+
+    if confidence > 1 or confidence < 0:
+        print("Condifence shoud be a number between 0 and 1. Ex. 0.1 or 0.2")
+        sys.exit()
+
     multiclass = CustomMulticlassifier(configuration_path)
-    parser.add_argument("-w", "--window", help="Maximum window to consider for the prediction.", default=multiclass.max_steps)
-    size_window = parser.parse_args().window
+
+    try:
+        size_window = int(parser.parse_args().window)
+    except:
+        print("Window Size shoud be a number")
+        sys.exit()
 
     report_file = open(os.path.join(os.path.dirname(os.getcwd()), "report.txt"), "w")
     extractor = Extractor()
@@ -51,7 +70,6 @@ if __name__ == '__main__':
     record_thread.start()
     print("Start to Record")
     array_features = np.empty((0, 128))
-    confidence = 0.3
 
     time.sleep(2)
 
@@ -60,9 +78,10 @@ if __name__ == '__main__':
         time.sleep(1)
         features = extractor.extract_audioset_embedding("test{}.wav".format(count))
         array_features = np.append(array_features, features, axis=0)
-        if multiclass.min_steps < array_features.shape[0]:
+        if multiclass.min_steps <= array_features.shape[0]:
             prediction, accuracy = multiclass.predict(array_features)
-            print("{} - {}".format(labels[prediction], accuracy))
+            if accuracy > confidence:
+                print("{} - Acc. {}".format(labels[prediction], accuracy))
 
         if np.shape(array_features)[0] == size_window:
             array_features = np.delete(array_features, 0, 0)
